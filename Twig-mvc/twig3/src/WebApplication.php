784c0144a22db60a758af53dc1c4ae773187be88
Pawel <14218944+pwodyk@users.noreply.github.com>
2020-02-26 15:06:22 +0000

    completed twig3 excercise

---
+++ Twig-mvc/twig3/src/WebApplication.php
@@ -0,0 +1,29 @@
+<?php
+namespace TUDublin;
+
+class WebApplication
+{
+    const PATH_TO_TEMPLATES = __DIR__ . '/../templates';
+    private $mainController;
+
+    public function __construct()
+    {
+        $twig = new \Twig\Environment(new \Twig\Loader\FilesystemLoader(self::PATH_TO_TEMPLATES));
+        $this->mainController = new MainController($twig);
+    }
+
+    public function run()
+    {
+        $action = filter_input(INPUT_GET, 'action');
+
+        switch ($action) {
+            case 'jokes':
+                $this->mainController->jokesAction();
+                break;
+            case 'index':
+            default:
+                $this->mainController->indexAction();
+                break;
+        }
+    }
+}
