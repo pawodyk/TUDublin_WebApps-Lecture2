9938279a34b6178f848584314688b482927a21f9
Pawel <14218944+pwodyk@users.noreply.github.com>
2020-02-26 11:32:09 +0000

    twig exercise initial commit

---
+++ Twig-mvc/twig1/src/MainController.php
@@ -0,0 +1,17 @@
+<?php
+namespace TUDublin;
+
+class MainController
+{
+    public function indexAction()
+    {
+        $pageTitle = 'home';
+        include __DIR__ . '/../templates/home.php';
+    }
+
+    public function jokesAction()
+    {
+        $pageTitle = 'jokes';
+        include __DIR__ . '/../templates/jokes.php';
+    }
+}
